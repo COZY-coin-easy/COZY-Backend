5e1f75ec1bfe9dae002d286e1e40ad6f09905a67
기웅민 <kiwoongmin@giungmin-ui-MacBookPro.local>
2022-02-06 17:50:32 +0900

    feature: 웹소켓 빗썸, 클라이언트 연결

--- app.js
+++ app.js
@@ -2,21 +2,29 @@ require("dotenv").config();
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 const join = require("./routes/join");
 const users = require("./routes/users");
+const socket = require("./routes/socket");
 
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", join);
 app.use("/users", users);
+app.use("/socket", socket);
 
 app.use(function (req, res, next) {
   next(createError(404));
@@ -31,4 +39,3 @@ app.use(function (err, req, res, next) {
 });
 
 module.exports = app;
-//develop 브랜치 생성
---
+++ routes/socket.js
@@ -0,0 +1,47 @@
+const WebSocket = require("ws");
+const wsModule = require("ws");
+
+module.exports = (server) => {
+  const wss = new WebSocket.Server({ server });
+  const bithumbWs = new wsModule("wss://pubwss.bithumb.com/pub/ws");
+  let coinInfo = null;
+
+  const requestData = {
+    type: "ticker",
+    symbols: ["BTC_KRW"],
+    tickTypes: ["24H"],
+  };
+  const subscribeData = JSON.stringify(requestData);
+
+  bithumbWs.onopen = () => {
+    bithumbWs.send(subscribeData);
+  };
+
+  bithumbWs.onmessage = (event) => {
+    coinInfo = event.data;
+  };
+
+  wss.on("clientConnection", (ws) => {
+    ws.on("message", (message) => {
+      console.log("클라이언트에서 보내온 메세지:::", message.toString("utf8"));
+    });
+
+    ws.on("error", (error) => {
+      console.error(error);
+    });
+
+    ws.interval = () => {
+      setTimeout(() => {
+        try {
+          ws.send(coinInfo);
+        } catch (error) {
+          console.error(error);
+        }
+
+        ws.interval();
+      }, 1000);
+    };
+
+    ws.interval();
+  });
+};
